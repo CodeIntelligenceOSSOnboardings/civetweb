6e216d04e999830f580ac1c0a131b5138cb7cc35
0roman <wagner@code-intelligence.com>
2023-11-23 03:46:17 +0100

    fine tune GET fuzzing example

--- examples/rest/rest_fuzz.cc
+++ examples/rest/rest_fuzz.cc
@@ -1,171 +1,34 @@
-// Includes section
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <unistd.h>
-#endif
-
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
-#include "cJSON.h"
 #include "civetweb.h"
-#include <assert.h>
+
 #include <cifuzz/cifuzz.h>
 #include <fuzzer/FuzzedDataProvider.h>
 
-// Constants
-#define PORT "8089"
-#define HOST_INFO "http://localhost:8089"
-#define EXAMPLE_URI "/res/*/*"
-#define EXAMPLE_FUZZ "/fuzz"
-#define EXIT_URI "/exit"
 #define HOST "localhost"
+#define PORT "8089"
+#define VULN_ENDPOINT "/fuzz"
 
-// Global variables
-int exitNow = 0;
-static unsigned request = 0; // Demo data: request counter
+struct mg_callbacks callbacks;
+struct mg_context *ctx;
 
-// Forward declarations
-static int SendJSON(struct mg_connection *conn, cJSON *json_obj);
-static int
-ExampleGET(struct mg_connection *conn, const char *p1, const char *p2);
-static int
-ExampleDELETE(struct mg_connection *conn, const char *p1, const char *p2);
-static int
-ExamplePUT(struct mg_connection *conn, const char *p1, const char *p2);
-static int FuzzHandler(struct mg_connection *conn, void *cbdata);
-static int ExampleHandler(struct mg_connection *conn, void *cbdata);
-static int ExitHandler(struct mg_connection *conn, void *cbdata);
-static int log_message(const struct mg_connection *conn, const char *message);
+int server_started = false;
+static int VulnEndpointHandler(struct mg_connection *conn, void *cbdata);
 
 
 static int
-SendJSON(struct mg_connection *conn, cJSON *json_obj)
-{
-	char *json_str = cJSON_PrintUnformatted(json_obj);
-	size_t json_str_len = strlen(json_str);
-
-	/* Send HTTP message header (+1 for \n) */
-	mg_send_http_ok(conn, "application/json; charset=utf-8", json_str_len + 1);
-
-	/* Send HTTP message content */
-	mg_write(conn, json_str, json_str_len);
-
-	/* Add a newline. This is not required, but the result is more
-	 * human-readable in a debugger. */
-	mg_write(conn, "\n", 1);
-
-	/* Free string allocated by cJSON_Print* */
-	cJSON_free(json_str);
-
-	return (int)json_str_len;
-}
-
-
-static int
-ExampleGET(struct mg_connection *conn, const char *p1, const char *p2)
-{
-	cJSON *obj = cJSON_CreateObject();
-
-	if (!obj) {
-		/* insufficient memory? */
-		mg_send_http_error(conn, 500, "Server error");
-		return 500;
-	}
-
-	printf("GET %s/%s\n", p1, p2);
-	cJSON_AddStringToObject(obj, "version", CIVETWEB_VERSION);
-	cJSON_AddStringToObject(obj, "path1", p1);
-	cJSON_AddStringToObject(obj, "path2", p2);
-	cJSON_AddNumberToObject(obj, "request", ++request);
-	SendJSON(conn, obj);
-	cJSON_Delete(obj);
-
-	return 200;
-}
-
-
-static int
-ExampleDELETE(struct mg_connection *conn, const char *p1, const char *p2)
-{
-	printf("DELETE %s/%s\n", p1, p2);
-	mg_send_http_error(conn,
-	                   204,
-	                   "%s",
-	                   ""); /* Return "deleted" = "204 No Content" */
-
-	return 204;
-}
-
-
-static int
-ExamplePUT(struct mg_connection *conn, const char *p1, const char *p2)
-{
-	char buffer[1024];
-	int dlen = mg_read(conn, buffer, sizeof(buffer) - 1);
-	cJSON *obj, *elem;
-	unsigned newvalue;
-
-	printf("PUT %s/%s\n", p1, p2);
-	if ((dlen < 1) || ((unsigned long)dlen >= sizeof(buffer))) {
-		mg_send_http_error(conn, 400, "%s", "No request body data");
-		return 400;
-	}
-	buffer[dlen] = 0;
-
-	obj = cJSON_Parse(buffer);
-	if (obj == NULL) {
-		mg_send_http_error(conn, 400, "%s", "Invalid request body data");
-		return 400;
-	}
-
-	elem = cJSON_GetObjectItemCaseSensitive(obj, "request");
-
-	if (!cJSON_IsNumber(elem)) {
-		cJSON_Delete(obj);
-		mg_send_http_error(conn,
-		                   400,
-		                   "%s",
-		                   "No \"request\" number in body data");
-		return 400;
-	}
-
-	newvalue = (unsigned)elem->valuedouble;
-
-	if ((double)newvalue != elem->valuedouble) {
-		cJSON_Delete(obj);
-		mg_send_http_error(conn,
-		                   400,
-		                   "%s",
-		                   "Invalid \"request\" number in body data");
-		return 400;
-	}
-
-	request = newvalue;
-	cJSON_Delete(obj);
-
-	mg_send_http_error(conn, 201, "%s", ""); /* Return "201 Created" */
-
-	return 201;
-}
-
-static int
-FuzzHandler(struct mg_connection *conn, void *cbdata)
+VulnEndpointHandler(struct mg_connection *conn, void *cbdata)
 {
 	char path1[1024], path2[1024];
 	const struct mg_request_info *ri = mg_get_request_info(conn);
 	struct mg_match_context mcx;
 	mcx.case_sensitive = 0;
-	mg_match(EXAMPLE_FUZZ, ri->local_uri, &mcx);
+	mg_match(VULN_ENDPOINT, ri->local_uri, &mcx);
 	memcpy(path1, mcx.match[0].str, mcx.match[0].len);
 	path1[mcx.match[0].len] = 0;
 	memcpy(path2, mcx.match[1].str, mcx.match[1].len);
 	path2[mcx.match[1].len] = 0;
 
-	if (strcmp(ri->local_uri, EXAMPLE_FUZZ) != 0) {
+	if (strcmp(ri->local_uri, VULN_ENDPOINT) != 0) {
 		mg_send_http_error(conn, 404, "Invalid path: %s\n", ri->local_uri);
 		return 404;
 	}
@@ -180,10 +43,10 @@ FuzzHandler(struct mg_connection *conn, void *cbdata)
 			mg_send_http_error(conn, 400, "Query string too short");
 			return 400;
 		}
-		fprintf(stderr, "Query string: %s\n", ri->query_string);
+		//fprintf(stderr, "Query string: %s\n", ri->query_string);
 		//   Vulnerability simulation for the specific query 'q=fuzz'
-		if (strncmp(ri->query_string, "q=fu", 4) == 0) {
-			char buffer[32]; // This is a small local buffer
+		if (strncmp(ri->query_string, "q=fuzz", 6) == 0) {
+			char buffer[64]; // This is a small local buffer
 
 			// Vulnerability: We are copying the entire query_string into the
 			// local buffer without checking its length
@@ -191,7 +54,6 @@ FuzzHandler(struct mg_connection *conn, void *cbdata)
 
 			mg_send_http_ok(conn, "text/plain", strlen(buffer));
 			mg_write(conn, buffer, strlen(buffer));
-			// return ExampleGET(conn, path1, path2);
 			return 200;
 		}
 		mg_send_http_error(conn,
@@ -199,99 +61,14 @@ FuzzHandler(struct mg_connection *conn, void *cbdata)
 		                   "Invalid query string: %s\n",
 		                   ri->query_string);
 		return 400;
-	} else if ((0 == strcmp(ri->request_method, "PUT"))
-	           || (0 == strcmp(ri->request_method, "POST"))
-	           || (0 == strcmp(ri->request_method, "PATCH"))) {
-		/* In this example, do the same for PUT, POST and PATCH */
-		return ExamplePUT(conn, path1, path2);
 	}
-	if (0 == strcmp(ri->request_method, "DELETE")) {
-
-		return ExampleDELETE(conn, path1, path2);
-	}
-
-	/* this is not a GET request */
-	mg_send_http_error(
-	    conn, 405, "Only GET, PUT, POST, DELETE and PATCH method supported");
+	mg_send_http_error(conn, 405, "Only GET method supported");
 	return 405;
 }
 
-
-static int
-ExampleHandler(struct mg_connection *conn, void *cbdata)
-{
-	char path1[1024], path2[1024];
-	const struct mg_request_info *ri = mg_get_request_info(conn);
-	const char *url = ri->local_uri;
-	size_t url_len = strlen(url);
-
-	/* Pattern matching */
-	struct mg_match_context mcx;
-	mcx.case_sensitive = 0;
-	ptrdiff_t ret = mg_match(EXAMPLE_URI, url, &mcx);
-	if (((unsigned long)ret != url_len) || (mcx.num_matches != 2)) {
-		/* Note: Could have done this with a $ at the end of the match
-		 * pattern as well. Then we would have to check for a return value
-		 * of -1 only. Here we use this version as minimum modification
-		 * of the existing code. */
-		printf("Match ret: %i\n", (int)ret);
-		mg_send_http_error(conn, 404, "Invalid path: %s\n", url);
-		return 404;
-	}
-	memcpy(path1, mcx.match[0].str, mcx.match[0].len);
-	path1[mcx.match[0].len] = 0;
-	memcpy(path2, mcx.match[1].str, mcx.match[1].len);
-	path2[mcx.match[1].len] = 0;
-
-
-	(void)cbdata; /* currently unused */
-
-	/* According to method */
-	if (0 == strcmp(ri->request_method, "GET")) {
-		return ExampleGET(conn, path1, path2);
-	}
-	if ((0 == strcmp(ri->request_method, "PUT"))
-	    || (0 == strcmp(ri->request_method, "POST"))
-	    || (0 == strcmp(ri->request_method, "PATCH"))) {
-		/* In this example, do the same for PUT, POST and PATCH */
-		return ExamplePUT(conn, path1, path2);
-	}
-	if (0 == strcmp(ri->request_method, "DELETE")) {
-		return ExampleDELETE(conn, path1, path2);
-	}
-
-	/* this is not a GET request */
-	mg_send_http_error(
-	    conn, 405, "Only GET, PUT, POST, DELETE and PATCH method supported");
-	return 405;
-}
-
-
-static int
-ExitHandler(struct mg_connection *conn, void *cbdata)
-{
-	mg_printf(conn,
-	          "HTTP/1.1 200 OK\r\nContent-Type: "
-	          "text/plain\r\nConnection: close\r\n\r\n");
-	mg_printf(conn, "Server will shut down.\n");
-	mg_printf(conn, "Bye!\n");
-	exitNow = 1;
-	return 1;
-}
-
-
-static int
-log_message(const struct mg_connection *conn, const char *message)
-{
-	puts(message);
-	return 1;
-}
-
-struct mg_callbacks callbacks;
-struct mg_context *ctx;
 
 int
-one_time_setup()
+setup_server()
 {
 	const char *options[] = {"listening_ports",
 	                         PORT,
@@ -304,76 +81,61 @@ one_time_setup()
 	/* Init libcivetweb. */
 	mg_init_library(0);
 
-	/* Callback will print error messages to console */
-	// memset(&callbacks, 0, sizeof(callbacks));
-	// callbacks.log_message = log_message;
-
 	/* Start CivetWeb web server */
 	ctx = mg_start(&callbacks, 0, options);
 
-	/* Check return value: */
 	if (ctx == NULL) {
 		fprintf(stderr, "Cannot start CivetWeb - mg_start failed.\n");
 		return EXIT_FAILURE;
 	}
 
-	/* Add handler EXAMPLE_URI, to explain the example */
-	mg_set_request_handler(ctx, EXAMPLE_URI, ExampleHandler, 0);
-	mg_set_request_handler(ctx, EXIT_URI, ExitHandler, 0);
-	mg_set_request_handler(ctx, EXAMPLE_FUZZ, FuzzHandler, 0);
+	mg_set_request_handler(ctx, VULN_ENDPOINT, VulnEndpointHandler, 0);
 
-	/* Show some info */
-	printf("Start example: %s%s\n", HOST_INFO, EXAMPLE_URI);
-	printf("Fuzz example: %s%s\n", HOST_INFO, EXAMPLE_FUZZ);
-	printf("Exit example:  %s%s\n", HOST_INFO, EXIT_URI);
+	printf("Fuzz example: http://%s:%s%s\n", HOST, PORT, VULN_ENDPOINT);
 	return 0;
 }
 
-// FUZZ_TEST_SETUP()
-//{
-//	one_time_setup();
-// }
 
-
-int err = false;
 FUZZ_TEST(const uint8_t *data, size_t size)
 {
-	if (!err) {
-		one_time_setup();
-		err = true;
+	if (!server_started) {
+		setup_server();
+		server_started = true;
 	}
-	if (size < 50) {
-		return;
-	}
-
-	FuzzedDataProvider fuzzed_data(data, size);
 
 	char errbuf[256] = {0};
-	// NOTE: HOST and PORT could also be build from the FDP in a more
-	// sophisticated fuzz harness
-	struct mg_connection *cli =
+	struct mg_connection *client =
 	    mg_connect_client(HOST, atoi(PORT), 0, errbuf, sizeof(errbuf));
 
-	if (cli == NULL) {
+	if (client == NULL) {
 		fprintf(stderr, "Cannot connect client: %s\n", errbuf);
-		// err = false;
 		return;
 	}
 
-	std::string method =
-	    fuzzed_data.PickValueInArray({"GET", "POST", "PUT", "DELETE"});
-	// NOTE: Instead of only polling the `EXAMPLE_FUZZ` endpoint we could
+	//std::string method =
+	//    fuzzed_data.PickValueInArray({"GET", "POST", "PUT", "DELETE"});
+	// NOTE: Instead of only polling the `VULN_ENDPOINT` endpoint we could
 	// also build these endpoints by random or pick from a list of known good
 	// ones!
-	mg_printf(cli,
-	          "%s %s?q=%s HTTP/1.1\r\n",
-	          "GET", // method.c_str(),
-	          EXAMPLE_FUZZ,
-	          fuzzed_data.ConsumeRemainingBytesAsString().c_str());
-	mg_printf(cli, "Host: %s\r\n", HOST);
-	mg_printf(cli, "Connection: close\r\n\r\n");
 
-	mg_get_response(cli, errbuf, sizeof(errbuf), -1);
-	mg_close_connection(cli);
+	FuzzedDataProvider fuzzed_data(data, size);
+	char* method = "GET";
+	char* path = "/fuzz";
+	char* parameter_name = "q";
+	std::string input = fuzzed_data.ConsumeRemainingBytesAsString();
+	char * parameter_value = &input[0];
+
+	mg_printf(client,
+	          "%s %s?%s=%s HTTP/1.1\r\n",
+	          method,
+	          path,
+			  parameter_name,
+	          parameter_value);
+	mg_printf(client, "Host: %s\r\n", HOST);
+	mg_printf(client, "Connection: close\r\n\r\n");
+
+	mg_get_response(client, errbuf, sizeof(errbuf), -1);
+	mg_close_connection(client);
+
 	return;
 }
